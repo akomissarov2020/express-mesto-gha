8158a9c2b6d68dd8b615f60a5a8414be586a47d6
Aleksey Komissarov <ad3002@gmail.com>
2022-02-20 23:13:53 +0300

    feat/tune eslint

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,7 +13,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
